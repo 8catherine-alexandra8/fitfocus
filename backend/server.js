2923ecd6d87a8d66d79f4121fc1a25cf89d8f029
Catherine Alexandra <8catherine.alexandra8@gmail.com>
2021-02-11 16:09:13 -0700

    prep for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
@@ -13,9 +14,18 @@ connectDB()
 const app = express()
 app.use(express.json())
 
-app.get('/', (req, res) => {
-	res.send('API is running')
-})
+const __dirname = path.resolve()
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/client/build')))
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	)
+} else {
+	app.get('/', (req, res) => {
+		res.send('API is running')
+	})
+}
 
 app.use('/api/settings', settingRoutes)
 app.use('/api/exercises', exerciseRoutes)
